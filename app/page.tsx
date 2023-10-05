74bb1e22129a492f3a790d69dffe2bf4336fdb51
Akash Chawdhery <chaudharyakash1422@gmail.com>
2023-10-05 20:40:37 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-   <div>
-    <h1> Hello </h1>
+   <div className='text-green-500 text-3xl'>
+   Hello AI Replica
    </div>
   )
 }
